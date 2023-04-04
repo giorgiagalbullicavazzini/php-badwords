5d742fd742d21a1d2efa7e2023553408d22a3863
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-04-04 15:00:47 +0200

    print paragraph and its length

--- form.php
+++ form.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <div class="chosen-paragraph">
+        <div class="label">Testo inserito: <span class="text">"<?php echo $_POST['paragraph']; ?>"</span></div>
+        <div class="length">Lunghezza del testo: <?php echo strlen($_POST['paragraph']); ?> caratteri</div>
+    </div>
+</body>
+
+</html>
